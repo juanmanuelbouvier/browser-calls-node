3f3667fd596f209ee04932b62fc2ff047417e3c9
juanmanuel.bouvier <juanmanuel.bouvier@almundo.com>
2018-06-22 18:43:41 -0300

    Voice welcome and recording on browser-to-browser calls

--- routes/call.js
+++ routes/call.js
@@ -8,8 +8,18 @@ router.post('/connect', twilio.webhook({validate: false}), function(req, res, ne
   var phoneNumber = req.body.phoneNumber;
   var callerId = process.env.TWILIO_PHONE_NUMBER;
   var twiml = new VoiceResponse();
+  twiml.say(
+      {
+          voice: 'alice',
+          language: 'es-MX',
+      },
+      'No me preguntes, sólo soy una demo'
+  );
 
-  var dial = twiml.dial({callerId : callerId});
+  var dial = twiml.dial({
+      callerId : callerId,
+      record: 'record-from-answer-dual'
+  });
   if (phoneNumber != null) {
     dial.number(phoneNumber);
   } else {
